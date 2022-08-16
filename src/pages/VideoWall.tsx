ef9bea395f8bec2b2c7d8c0f430cbb17ecb72717
B6108755 <buasinggg@gmail.com>
2022-08-16 16:00:25 +0700

    Tailwind and VideoWall page Updated

--- src/pages/VideoWall.tsx
+++ src/pages/VideoWall.tsx
@@ -20,5 +20,17 @@ const people = [
 ];
 
 export default function Example() {
-  return <></>;
+  return (
+    <ul className="divide-y divide-gray-200">
+      {people.map((person) => (
+        <li key={person.email} className="py-4 flex">
+          <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+          <div className="ml-3">
+            <p className="text-sm text-white-400 font-medium">{person.name}</p>
+            <p className="text-sm text-gray-500">{person.email}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 }
